c5e38ebf95c73b7085de1f95c1db445ac3b2dfe3
Robin Steeman <Robin_steeman@hotmail.com>
2020-06-19 11:12:08 +0200

    working version

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import 'antd/dist/antd.css';
 
-import Dashboard from './Container/Dashboard/Dashboard';
-import Store from "./Container/Dashboard/Store"
+import Dashboard from './Dashboard';
+import Store from './Store';
 
 function App() {
-  return (
-    <div className="App">
-      <Store>
-        <Dashboard />
-      </Store>
-    </div>
-  );
+   return (
+      <div className='App'>
+         <Store>
+            <Dashboard />
+         </Store>
+      </div>
+   );
 }
 
 export default App;
--- src/server/index.js
+++ src/server/index.js
@@ -2,16 +2,18 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+// app.get('/', function (req, res) {
+//    res.sendFile(__dirname + '/index.html');
+// });
+
+io.on('connection', socket => {
+   console.log('a user connected');
+   socket.on('chat message', msg => {
+      console.log('message: ' + JSON.stringify(msg));
+      io.emit('chat message', msg);
+   });
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-
-http.listen(3001, function(){
-  console.log('listening on *:3001');
+http.listen(3001, () => {
+   console.log('listening on *:3001');
 });
